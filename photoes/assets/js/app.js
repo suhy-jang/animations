2b977b000b36f4d52aca89ff99650007d83ad161
shjang7 <lucy.sh.jang@gmail.com>
2019-10-22 16:06:38 +0900

    Update dot navigator

--- photoes/assets/js/app.js
+++ photoes/assets/js/app.js
@@ -11,28 +11,50 @@ const leftArrow = document.querySelector('.fa-arrow-left');
 const rightArrow = document.querySelector('.fa-arrow-right');
 const innerFrame = document.querySelector('.container-inner');
 const numOfImage = 6;
-let slide = 0;
 
-const calculateTransform = () => {
-  return `translateX(calc(calc(-100% * ${slide}) / ${numOfImage}))`;
+const updateSlideByDx = (inSlide, dx) => {
+  // console.log('update', inSlide, dx);
+  let updateSlide = inSlide;
+  if (updateSlide + dx < 0) {
+    updateSlide += numOfImage;
+  } else if (updateSlide + dx >= numOfImage) {
+    updateSlide -= numOfImage;
+  }
+  return updateSlide + dx;
 }
 
-const [leftDot, rightDot] = document.querySelectorAll('.circle-side');
+const swapMainAndSide = ({ newMainId }) => {
+  const originalMain = document.querySelector('.circle-main');
+  originalMain.classList.remove('circle-main');
+  originalMain.classList.add('circle-side');
+  
+  const newMain = document.getElementById(`circle-${newMainId}`);
+  newMain.classList.remove('circle-side');
+  newMain.classList.add('circle-main');
+}
 
-[leftArrow, leftDot].forEach(e => {
-  e.addEventListener('click', () => {
-    if (slide - 1 >= 0) {
-      slide -= 1;
-      innerFrame.style.transform = calculateTransform();
-    }
-  });
+let slide = 0;
+const doSlide = ({ newSlide, dx }) => {
+  if (dx) slide = updateSlideByDx(slide, dx);
+  if (newSlide) slide = newSlide;
+  swapMainAndSide({ newMainId: slide });
+
+  innerFrame.style.transform = `translateX(calc(calc(-100% * ${slide}) / ${numOfImage}))`;
+}
+
+const nextScreen = setInterval(() => { doSlide({ dx: 1 }) }, 5000);
+
+leftArrow.addEventListener('click', () => {
+  doSlide({ dx: -1 });
 });
 
-[rightArrow, rightDot].forEach(e => {
-  e.addEventListener('click', () => {
-    if (slide + 1 < numOfImage) {
-      slide += 1;
-      innerFrame.style.transform = calculateTransform();
-    }
-  });
+rightArrow.addEventListener('click', () => {
+  doSlide({ dx: 1 });
 });
+
+const dots = document.querySelectorAll('.circle');
+dots.forEach(dot => {
+  dot.addEventListener('click', () => {
+    doSlide({ newSlide: Number(dot.id.split('-')[1]) });
+  })
+})
