eeed8ceb5b6c3df45ac209e59d3f385d8cdce450
shjang7 <lucy.sh.jang@gmail.com>
2019-10-22 16:13:50 +0900

    Move image order

--- photoes/assets/js/app.js
+++ photoes/assets/js/app.js
@@ -1,19 +1,9 @@
-const imageSources = [
-  './assets/images/wine-1761613_640.jpg',
-  './assets/images/grapes-2656259_640.jpg',
-  './assets/images/red-wine-2443699_640.jpg',
-  './assets/images/grapes-1659118_640.jpg',
-  './assets/images/sparkling-wine-1030754_640.jpg',
-  './assets/images/purple-grapes-553464_640.jpg',
-];
-
 const leftArrow = document.querySelector('.fa-arrow-left');
 const rightArrow = document.querySelector('.fa-arrow-right');
 const innerFrame = document.querySelector('.container-inner');
 const numOfImage = 6;
 
 const updateSlideByDx = (inSlide, dx) => {
-  // console.log('update', inSlide, dx);
   let updateSlide = inSlide;
   if (updateSlide + dx < 0) {
     updateSlide += numOfImage;
@@ -21,17 +11,17 @@ const updateSlideByDx = (inSlide, dx) => {
     updateSlide -= numOfImage;
   }
   return updateSlide + dx;
-}
+};
 
 const swapMainAndSide = ({ newMainId }) => {
   const originalMain = document.querySelector('.circle-main');
   originalMain.classList.remove('circle-main');
   originalMain.classList.add('circle-side');
-  
+
   const newMain = document.getElementById(`circle-${newMainId}`);
   newMain.classList.remove('circle-side');
   newMain.classList.add('circle-main');
-}
+};
 
 let slide = 0;
 const doSlide = ({ newSlide, dx }) => {
@@ -40,9 +30,9 @@ const doSlide = ({ newSlide, dx }) => {
   swapMainAndSide({ newMainId: slide });
 
   innerFrame.style.transform = `translateX(calc(calc(-100% * ${slide}) / ${numOfImage}))`;
-}
+};
 
-const nextScreen = setInterval(() => { doSlide({ dx: 1 }) }, 5000);
+const nextScreen = setInterval(() => { doSlide({ dx: 1 }); }, 5000);
 
 leftArrow.addEventListener('click', () => {
   doSlide({ dx: -1 });
@@ -56,5 +46,5 @@ const dots = document.querySelectorAll('.circle');
 dots.forEach(dot => {
   dot.addEventListener('click', () => {
     doSlide({ newSlide: Number(dot.id.split('-')[1]) });
-  })
-})
+  });
+});
